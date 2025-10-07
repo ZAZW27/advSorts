d5909c3992684f425851e0ddb7d6929294bc64e9
ZAZW27 <clawsrage27@gmail.com>
2025-10-07 10:37:06 +0800

    Creating merge sort

--- src/sorts/Merge.java
+++ src/sorts/Merge.java
@@ -1,4 +1,14 @@
 package sorts;
+import java.io.*;
 
 public class Merge {
+    static void merge(int[] arr){
+        int length = arr.length;
+        if (length <= 1)return;
+
+        int middle = (length / 2);
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+
+    }
 }
